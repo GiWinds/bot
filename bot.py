104ac9f75fe63461777db3a993d7f6c1c042a6c1
GiWinds <h0043046@gmail.com>
2023-01-27 20:52:40 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,8 @@
+import asyncio
 import discord
 from discord.ext import commands
 import json
+import os
 
 with open('setting.json', 'r', encoding='utf8') as jfile:
     jdata = json.load(jfile)
@@ -11,12 +13,33 @@ bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.event
 async def on_ready():
-    print("Bot is online")
+    print("Bot is online!")
 
 @bot.command()
-async def picture(ctx):
-    pic = discord.File("C:\\Users\\jason\\Downloads\\test.png")
-    await ctx.send(file=pic)
+async def load(ctx, extension):
+    bot.load_extension(f'cmds.{extension}')
+    await ctx.send(f'Loaded {extension} done.')
 
+@bot.command()
+async def unload(ctx, extension):
+    bot.unload_extension(f'cmds.{extension}')
+    await ctx.send(f'Un-Loaded {extension} done.')
 
-bot.run(jdata['TOKEN'])
+@bot.command()
+async def reload(ctx, extension):
+    bot.reload_extension(f'cmds.{extension}')
+    await ctx.send(f'Re-Loaded {extension} done.')
+
+async def load_extensions():
+    for filename in os.listdir('./cmds'):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'cmds.{filename[:-3]}')
+
+async def main():
+    async with bot:
+        await load_extensions()
+        await bot.start(jdata['TOKEN'])
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
