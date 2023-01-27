880ead2b16c91b38b776d50588d0311d9f9d2e90
GiWinds <h0043046@gmail.com>
2023-01-27 17:46:37 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,12 +1,22 @@
 import discord
 from discord.ext import commands
+import json
+
+with open('setting.json', 'r', encoding='utf8') as jfile:
+    jdata = json.load(jfile)
 
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix = '.', intents = intents)
+bot = commands.Bot(command_prefix='.', intents=intents)
 
 @bot.event
 async def on_ready():
     print("Bot is online")
 
-bot.run('MTA2ODQzMDgyNDc4NzE2OTMxMA.GrlhYO.YpV3q8vR94NNrSE9xzI_gBySTUGbp93m_Dtwf0')
+@bot.command()
+async def picture(ctx):
+    pic = discord.File("C:\\Users\\jason\\Downloads\\test.png")
+    await ctx.send(file=pic)
+
+
+bot.run(jdata['TOKEN'])
